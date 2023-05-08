af9875eba1a6629c5aae01672ce9226b380163d2
Adam Polner <polnera@rsd407.org>
2023-05-08 08:34:40 -0700

    Sprites don't fit in one sprite for the tutorial

--- MyGame/Tutorial Text.cs
+++ MyGame/Tutorial Text.cs
@@ -17,16 +17,16 @@ namespace MyGame
         private const int clickdelay = 10;
         private readonly Text _text = new Text();
         private readonly Sprite sprite = new Sprite();
-        private readonly Sprite sprite2 = new Sprite();
+
         public Tutorial_Text()
         {//UI
             _text.Font = Game.GetFont("../../../Resources/times new roman.ttf");
             
             _text.Position = new Vector2f(200,500);
             _text.CharacterSize = 40;
-            sprite2.Texture= Game.GetTexture("../../../Resources/64X32tile.png");
-            sprite2.Position= new Vector2f(100, 500);
             _text.FillColor = Color.White;
+            sprite.Texture = Game.GetTexture("../../../Resources/64X32tile.png");
+            sprite.Position = new Vector2f(100, 500);
             _text.DisplayedString ="Click on a nearby tile to move";
             tutorialnum=0;
         }
@@ -34,17 +34,16 @@ namespace MyGame
         {
             Game.RenderWindow.Draw(_text);
             Game.RenderWindow.Draw(sprite);
-            Game.RenderWindow.Draw(sprite2);
         }
         public override void Update(Time elapsed)
         {
-            if (0<tutorialnum)
-            {
-            }
+    
             if (Mouse.IsButtonPressed(Mouse.Button.Left)&&clickdelay<=clicktimer&&tutorialnum==0)
             {
                 _text.Position = new Vector2f(1000, 200);
-                _text.DisplayedString = "Press space to fire a spell\nevery spell you cast decreases your health";
+                _text.DisplayedString = "Press space to fire a spell\nevery spell you cast decreases your health\nThis is the advanced spellbook it allows you to \ncast spells in all 8 directions";
+                sprite.Position = new Vector2f(900, 200);
+                sprite.Texture = Game.GetTexture("../../../Resources/spell book2.png");
                 clicktimer=0;
                 tutorialnum=1;
                 
